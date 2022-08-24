c377699e6e84ba6ab5e409c717757c659ff9027f
Sam Leonard <sam.leonard@nccgroup.com>
2022-08-24 17:26:03 +0100

    Version 1.0.0 release.

--- src/com/nccgroup/jwtreauth/utils/UrlComparison.java
+++ src/com/nccgroup/jwtreauth/utils/UrlComparison.java
@@ -1,5 +1,15 @@
-// Copyright 2022 Sam Leonard
-// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright 2022 NCC Group
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package com.nccgroup.jwtreauth.utils;
 
@@ -32,7 +42,8 @@ public class UrlComparison {
         // getHost can return null and therefore must be checked for the null case
         var aHost = a.getHost();
         var bHost = b.getHost();
-        if (aHost != null && bHost != null && !aHost.equals(bHost)) return false;
+        if (aHost == null ^ bHost == null) return false;
+        if (aHost != null && !aHost.equals(bHost)) return false;
 
         return a.getProtocol().equals(b.getProtocol());
     }
